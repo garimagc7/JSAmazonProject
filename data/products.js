ba34f636c241811f6ea892364d08001b10ab59ff
garimagc7 <garimagc7@gmail.com>
2026-09-13 15:51:56 +0545

    save product data

---
+++ data/products.js
@@ -0,0 +1,57 @@
+let products = [
+    {
+        id: P001,
+        image: "assets/Lunch_Box.png",
+        colors: "+52 other colors/patterns",
+        name: "Simple Kids Lunch Box",
+        description: "Insulated lunch box for kids",
+        stars: "★★★★☆",
+        reviews: "8,245",
+        priceCents: 1999
+    },{
+         id: P002,
+        image: "assets/Art_Supplies.png",
+        colors: "+5 other colors/patterns",
+        name: "Kids Art/Paint Brush",
+        description: "Complete art and drawing set for kids",
+        stars: "★★★★★",
+        reviews: "12,430",
+        priceCents: 2411
+    },{
+         id: P003,
+        image: "assets/Clothes.png",
+        colors: "+12 other colors/patterns",
+        name: "Kids Casual Clothing Set",
+        description: "Comfortable everyday clothes for kids",
+        stars: "★★★★☆",
+        reviews: "9,821",
+        priceCents: 2199
+    },{
+         id: P004,
+        image: "assets/product-picture1.png",
+        colors: "+10 other colors/patterns",
+        name: "School Backpack for Kids",
+        description: "Durable school backpack with multiple compartments",
+        stars: "★★★★☆",
+        reviews: "4,285",
+        priceCents: 2905
+    }, {
+         id: P005,
+        image: "assets/WaterBottle.png",
+        colors: "+18 other colors/patterns",
+        name: "Stainless Steel Water Bottle for Students",
+        description: "Insulated reusable water bottle, leakproof and durable",
+        stars: "★★★★★",
+        reviews: "6,842",
+        priceCents: 1616
+    }, {
+         id: P006,
+        image: "assets/FilesNFolder.png",
+        colors: "+8 other colors/patterns",
+        name: "School Files and Folders Set",
+        description: "Durable folders for school and office",
+        stars: "★★★★☆",
+        reviews: "3,921",
+        priceCents: 1889
+    }
+];
