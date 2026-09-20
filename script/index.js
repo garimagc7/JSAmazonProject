5617891d65da7d4ff131527b59c1b80630a0fb2d
garimagc7 <garimagc7@gmail.com>
2026-09-20 18:08:17 +0545

    modified

--- script/index.js
+++ script/index.js
@@ -3,7 +3,6 @@ import {homeCards} from '../data/homeData.js';
 let currentSlide = 0;
 
 const slide = document.querySelector('.slides');
-
 const totalSlides = document.querySelectorAll('.slides img').length;
 
 function showSlide() {
@@ -16,7 +15,6 @@ function nextSlide() {
     showSlide();
 
     if (currentSlide === totalSlides - 1) {
-
         setTimeout(() => {
             slide.style.transition = 'none';
             currentSlide = 0;
@@ -26,13 +24,11 @@ function nextSlide() {
                 slide.style.transition = 'transform 0.5s ease-in-out';
             }, 50);
         }, 500);
-
     }
 }
 
 function prevSlide() {
     if (currentSlide === 0) {
-
         slide.style.transition = 'none';
         currentSlide = totalSlides - 1;
         showSlide();
@@ -42,14 +38,12 @@ function prevSlide() {
             currentSlide--;
             showSlide();
         }, 50);
-
     } else {
         currentSlide--;
         showSlide();
     }
 }
 document.querySelector('.next').addEventListener('click', nextSlide);
-
 document.querySelector('.prev').addEventListener('click', prevSlide);
 
 const scrollBars = document.querySelectorAll('.products');
@@ -70,7 +64,9 @@ function generateHomeCards() {
     homeCards.forEach((card, index) => {
 
         if (index % 4 === 0) {
+
             const grid = document.createElement("div");
+
             grid.classList.add("product-grid");
 
             if (index >= 4) {
@@ -82,14 +78,18 @@ function generateHomeCards() {
 
         const grids = homeCardsContainer.querySelectorAll(".product-grid");
         const currentGrid = grids[grids.length - 1];
-D
+
         if (card.type === "single") {
+
             const cardElement = document.createElement("div");
+
             cardElement.classList.add("product-card");
 
             cardElement.innerHTML = `
                 <strong>${card.title}</strong>
+
                 <img src="${card.image}" alt="${card.title}">
+
                 <p>${card.linkText}</p>
             `;
 
@@ -97,16 +97,23 @@ D
         }
 
         else if (card.type === "grid") {
+
             const cardElement = document.createElement("div");
+
             cardElement.classList.add("product-card");
+
             cardElement.innerHTML = `
                 <strong>${card.title}</strong>
+
                 <div class="product-picture">
 
                     ${card.items.map(item => `
                         <div class="picture1">
+
                             <img src="${item.image}" alt="${item.text}">
+
                             <p>${item.text}</p>
+
                         </div>
                     `).join("")}
 
